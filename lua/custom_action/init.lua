649d38cff77c07a0937117ca17422e1d528901d3
mskasal <mustafasamedkasal@gmail.com>
2024-06-29 17:06:54 +0200

    fix

--- lua/custom_action/init.lua
+++ lua/custom_action/init.lua
@@ -21,15 +21,15 @@ end
 function M.setup()
 	-- Define a keymap for <leader>gct to execute custom action
 	vim.api.nvim_set_keymap(
-		"n",
+		"v",
 		"<leader>ct",
 		':lua require("custom_action").add_custom_comment_action()<CR>',
 		{ noremap = true, silent = true }
 	)
 
 	-- Optionally, define placeholders for <leader>gce and <leader>gcr
-	vim.api.nvim_set_keymap("n", "<leader>cr", "<Plug>(my_custom_placeholder_for_gce)", {})
-	vim.api.nvim_set_keymap("n", "<leader>ce", "<Plug>(my_custom_placeholder_for_gcr)", {})
+	vim.api.nvim_set_keymap("v", "<leader>cr", "<Plug>(my_custom_placeholder_for_gce)", {})
+	vim.api.nvim_set_keymap("v", "<leader>ce", "<Plug>(my_custom_placeholder_for_gcr)", {})
 end
 
 return M
