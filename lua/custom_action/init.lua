3447b4ed820bdd58de550b994faf1f5c1d221290
mskasal <mustafasamedkasal@gmail.com>
2024-06-29 16:39:43 +0200

    test

--- lua/custom_action/init.lua
+++ lua/custom_action/init.lua
@@ -2,23 +2,18 @@ local M = {}
 
 -- Function to add a comment on top of a code block and replace it
 function M.add_custom_comment_action()
-	-- Get the current buffer
 	local bufnr = vim.api.nvim_get_current_buf()
 
-	-- Get the selected range
 	local start_pos = vim.fn.getpos("'<")
 	local end_pos = vim.fn.getpos("'>")
 
 	local start_row = start_pos[2] - 1
 	local end_row = end_pos[2]
 
-	-- Get the lines in the selected range
 	local lines = vim.api.nvim_buf_get_lines(bufnr, start_row, end_row, false)
 
-	-- Add a custom comment
 	table.insert(lines, 1, "-- My custom comment")
 
-	-- Replace the selected lines with the modified lines
 	vim.api.nvim_buf_set_lines(bufnr, start_row, end_row, false, lines)
 end
 
@@ -26,25 +21,32 @@ end
 local function create_custom_action()
 	return {
 		title = "My Custom Action",
-		action = M.add_custom_comment_action,
+		action = function()
+			M.add_custom_comment_action()
+		end,
 	}
 end
 
 -- Register the custom action with LSP code actions
 function M.register_custom_action(client, bufnr)
-	local function custom_code_action_handler(_, _, params, client_id, _, _)
+	local function custom_code_action_handler(params)
 		local actions = {}
 		local mode = vim.api.nvim_get_mode().mode
-		if mode == "v" or mode == "V" or mode == "" then -- Check if in visual mode
+		if mode == "v" or mode == "V" or mode == "\x16" then -- Check if in visual mode
 			table.insert(actions, create_custom_action())
 		end
-		vim.lsp.util.apply_text_edits(actions, bufnr, client.offset_encoding)
 		return actions
 	end
 
-	client.handlers["textDocument/codeAction"] = vim.lsp.with(custom_code_action_handler, {})
+	client.config.commands = client.config.commands or {}
+	client.config.commands["CustomCodeAction"] = custom_code_action_handler
+
+	vim.api.nvim_buf_create_user_command(bufnr, "CustomCodeAction", function()
+		vim.lsp.buf.execute_command({ command = "CustomCodeAction" })
+	end, { range = true, desc = "Run custom code action" })
 end
 
+-- Register a custom command
 function M.setup()
 	vim.api.nvim_create_user_command("AddCustomComment", M.add_custom_comment_action, { range = true })
 end
